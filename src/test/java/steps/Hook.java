263d4bb99e418976c09afaf0ccc82762d2f717ee
David Letrán <davidletrang@gmail.com>
2017-01-02 12:17:21 +0100

    Added runner to run tests from maven

--- src/test/java/steps/Hook.java
+++ src/test/java/steps/Hook.java
@@ -1,11 +1,19 @@
 package steps;
 
 import cucumber.api.Scenario;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import utils.BaseUtils;
 import cucumber.api.java.After;
 import cucumber.api.java.Before;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.HashMap;
 
 /**
@@ -20,12 +28,28 @@ public class Hook extends BaseUtils{
     }
 
     @Before
-    public void initializeTest(Scenario scenario){
+    public void initializeTest(){
         base.data = new HashMap<String, Object>();
     }
 
     @After
-    public void tearDownTest(){
+    public void tearDownTest(Scenario scenario){
+        //Check failure
+        if(scenario.isFailed()){
+            File screenshot =  ((TakesScreenshot)base.driver).getScreenshotAs(OutputType.FILE);
+            Calendar calendar = Calendar.getInstance();
+            String dateFormat = new SimpleDateFormat("MM_dd_yyyy_HH_mm_ss").format(calendar.getTime());
+            String filename = scenario.getName()+"_"+dateFormat+".png";
+            try {
+                FileUtils.copyFile(screenshot, new File("failure_screenshots\\"+filename));
+
+            } catch (IOException e) {
+                System.out.println("Failure taking screenshot for test: "+scenario.getName());
+            }
+        }
+
+        //Delete cookies
+        base.driver.manage().deleteAllCookies();
         //Close web browser
         System.out.print("\nClosing the browser:"+base.data.get("browser"));
         base.driver.quit();
